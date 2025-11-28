2105969cc8e2804c35e835a8e868a7977a0d9277
dondie52 <114474977+dondie52@users.noreply.github.com>
2025-11-28 21:45:23 +0200

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["system-ui", "ui-sans-serif", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
